107526c69ccedfee2da94673cab195960ba08695
Amit <vamit20000@gmail.com>
2026-01-18 21:19:17 +0530

    fix: Ensure Metro correctly handles WASM files by removing them from asset extensions and confirming them as source extensions.

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,12 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push("wasm");
+const { resolver } = config;
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "wasm"),
+  sourceExts: [...resolver.sourceExts, "wasm"],
+};
 
 module.exports = config;
